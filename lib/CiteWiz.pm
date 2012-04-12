c335ab6aa311ce83193cae71715b64bdf18feeec
Dave Sherohman <Dave.Sherohman@lub.lu.se>
2012-04-12 14:12:56 +0200

    Reorganized URIs

--- lib/CiteWiz.pm
+++ lib/CiteWiz.pm
@@ -1,14 +1,20 @@
 package CiteWiz;
 use Dancer ':syntax';
 
+get '/' => sub {
+  redirect '/create';
+};
+
+get '/cite' => sub {
+  var page_title => 'Citation List';
+
+  template 'cite';
+};
+
 get '/create' => sub {
   var page_title => 'Embed Your Publication List in Your Homepage';
 
   template 'create';
-};
-
-get '/' => sub {
-  template 'index';
 };
 
 1;
