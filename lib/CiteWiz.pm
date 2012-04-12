ff30a32bd800324c70a2704f26b35e1176d0ff15
Dave Sherohman <Dave.Sherohman@lub.lu.se>
2012-04-12 13:52:39 +0200

    Renamed template for /create route from embed_form.tt to create.tt

--- lib/CiteWiz.pm
+++ lib/CiteWiz.pm
@@ -4,7 +4,7 @@ use Dancer ':syntax';
 get '/create' => sub {
   var page_title => 'Embed Your Publication List in Your Homepage';
 
-  template 'embed_form';
+  template 'create';
 };
 
 get '/' => sub {
