5129453336d807e5849e17291287e8a3125f42c6
Dave Sherohman <Dave.Sherohman@lub.lu.se>
2012-04-16 09:46:57 +0200

    Checkpoint

--- lib/RefMole/CiteList.pm
+++ lib/RefMole/CiteList.pm
@@ -4,7 +4,10 @@ use strict;
 use warnings;
 use 5.010;
 
-use Dancer 'config';
+use Dancer qw( config debug );
+
+use LWP::UserAgent;
+use XML::Simple;
 
 ### BEGIN ORMS LIFE-SUPPORT ###
 use lib config->{sbcat_path} . '/lib/extension';
@@ -14,6 +17,7 @@ use Orms;
 use Cwd;
 my $cfg;
 BEGIN {
+  $ENV{HOSTNAME} //= `hostname --fqdn`;
   my $original_path = getcwd;
   chdir config->{sbcat_path};
   $cfg = new luurCfg;
@@ -27,19 +31,16 @@ sub get_citations {
   my $conditions;
   my ($style, $sort_order, $hiddenlist);
 
-  my $limit = $param{limit} // 0;
-
   if ($param{author}) {
     $conditions = "_basic%20exact%20%22$param{author}%22";
 
     my $luur = Orms->new($cfg->{ormsCfg});
     my $o;
-    $o->{$_} = $luur->getObject($_) for qw( luAuthor personNumber );
-    ### TODO: Should 'personNumber' be changed to 'lucat' or something here?
+    $o->{$_} = $luur->getObject($_) for qw( luAuthor luLdapId );
     my ($luAuthorOId) = @{
       $luur->getObjectsByAttributeValues(
         type            => $o->{'luAuthor'},
-        attributeValues => {$o->{'personNumber'} => $param{author}}
+        attributeValues => {$o->{'luLdapId'} => $param{author}}
       )
     };
 
@@ -60,7 +61,98 @@ sub get_citations {
     return {};
   }
 
-  return {%param, conditions => $conditions,};
+  $conditions .= "AND%20PublishingYear%3D%22$param{publyear}%22"
+    if $param{publyear};
+
+  $conditions .= "AND%20documentType%3D%22$param{doctype}%22"
+    if $param{doctype};
+
+  my $query_url = config->{cite}{sru_url}
+    . "&query=$conditions&sortKeys=publishingYear,,0";
+  debug $query_url;
+
+  ### TODO: If unlimited result set sizes are needed, adapt "paging the result"
+  ### loop from lines 520-559 of bup_sru.pl
+  my $remaining = $param{limit} || config->{cite}{result_limit};
+  $query_url .= "&maximumRecords=$remaining";
+  my $sru_response = _get_sru_result($query_url);
+  debug $sru_response;
+  my $result = _extract_mods($sru_response);
+  debug $result;
+
+  if ($param{author}) {
+    $result->{norm_author} = _switch_author($param{author});
+    $result->{list_author} = $param{author};
+
+    ### TODO: Compare results against "delete hidden publications" loop @
+    ### bup_sru.pl 568-584
+    if ($hiddenlist) {
+      my %hidden = map { $_ => 1 } @$hiddenlist;
+      for (reverse 0 .. $result->{numrecs}) {
+        if ($hidden{$result->{records}[$_]{recordid}}) {
+          splice(@{$result->{records}}, $_, 1);
+        }
+      }
+    }
+  }
+
+  ### Continue here
+
+  return {
+    %param,
+    conditions => $conditions,
+    query_url  => $query_url,
+  };
+}
+
+sub _add_record_fields {
+  ### TODO: Implement based on buo_sru sub add_record_fields
+  return @_;
+}
+
+sub _extract_mods {
+  my ($mods_xml) = @_;
+
+  my $parser = XML::Simple->new;
+
+  ### Probably not needed thanks to Dancer, but it's in bup_sru.pl.
+  ### Uncomment if there are encoding issues.
+  #utf8::upgrade($mods_xml);
+
+  my $xml = $parser->XMLin( $mods_xml,
+    forcearray => [
+      'record', 'subject', 'relatedItem', 'detail', 'note', 'abstract',
+      'name', 'role', 'titleInfo', 'extent', 'identifier'
+    ]
+  );
+
+  my %result;
+  $result{numrecs} = $xml->{numberOfRecords};
+
+  for (@{$xml->{records}->{record}}) {
+    my $fields = _add_record_fields(\$_->{recordData});
+    push(@{$result{records}}, $_);
+  }
+
+  return \%result;
+}
+
+sub _get_sru_result {
+  my ($query_url) = @_;
+
+  my $ua = LWP::UserAgent->new();
+  $ua->agent('Netscape/4.75');
+  $ua->from('bd-tech@lub.lu.se');
+  $ua->timeout(60);
+  $ua->max_size(5000000);
+
+  my $req = HTTP::Request->new('GET', $query_url);
+  return $ua->request($req)->content;
+}
+
+sub _switch_author {
+  ### TODO: Implement based on bup_sru.pl sub switch_author
+  return @_;
 }
 
 1;
